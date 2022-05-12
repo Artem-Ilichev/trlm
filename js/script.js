e08074aa5c4639775c5e5f1d5dd3b92507ce8189
Artem-Ilichev <tematulskiy25@gmail.com>
2022-05-12 23:31:33 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const catalogBtn = document.querySelectorAll('.button-catalog');
-    const catalogMenu = document.querySelector('.menu-container');
+    const catalogMenu = document.querySelector('.catalog-menu');
+    const ctlgMenMinWidth = document.querySelector('.catalog-menu-tablet-mobile');
     const modalWindow = document.querySelector('.modal-window');
+    const registrationBtn = document.querySelector('.user-profile__enter');
+    const registration = document.querySelector('.registration');
+    const registrationClose = document.querySelector('.button-close');
 
         catalogBtn.forEach(function (item) {
         item.addEventListener('click',function (){
             item.classList.toggle('button-catalog--active')
             catalogMenu.classList.toggle('menu-container--open');
-            modalWindow.classList.toggle('modal-window--open');
+            ctlgMenMinWidth.classList.toggle('catalog-menu-tablet-mobile--open');
+            ModalWindow();
         })
     })
+    registrationBtn.addEventListener('click',function (){
+        registration.classList.toggle('registration--open');
+        ModalWindow();
+    })
+
+    registrationClose.addEventListener('click',function (){
+        registration.classList.remove('registration--open');
+        ModalWindow();
+    })
+
+    function ModalWindow() {
+           if (modalWindow.classList.contains('modal-window--open')){
+               modalWindow.classList.remove('modal-window--open')
+           } else {
+               modalWindow.classList.add('modal-window--open')
+           }
+    }
 })
